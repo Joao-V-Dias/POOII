308e0a6825e23697167f1be3144ccf5362ef5199
Joao Dias <100037973+Joao-V-Dias@users.noreply.github.com>
2025-08-04 22:10:17 -0300

    Add files via upload

---
+++ prjTesteJavaForms/src/main/java/com/mycompany/prjtestejavaforms/classes/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prjtestejavaforms.classes;
+
+/**
+ *
+ * @author IFTM
+ */
+public class Calculadora {
+    public Float somar(Float nro1, Float nro2){
+        return nro1 + nro2;
+    }
+    
+    public Float subtrair(Float nro1, Float nro2){
+        return nro1 - nro2;
+    }
+    
+    public Float dividir(Float nro1, Float nro2){
+        return nro1 / nro2;
+    }
+    
+    public Float multiplicar(Float nro1, Float nro2){
+        return nro1 * nro2;
+    }
+}
